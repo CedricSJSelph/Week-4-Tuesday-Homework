abe330eac7f7ce0dbcee9df9782c59ffdddea158
koastiebratt@gmail.com <you@example.com>
2020-08-22 14:30:38 -0400

    6.17 Complete

--- src/src/UnknownLengthPattern.java
+++ src/src/UnknownLengthPattern.java
@@ -1,10 +1,39 @@
 package src;
 
+import java.util.Scanner;
+
 public class UnknownLengthPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter a value for n: ");
+		
+		int n = input.nextInt();
+		
+		printMatrix(n);
+		
+	}
+	
+	public static void printMatrix(int n) {
+		
+		System.out.println();
+		
+		//Rows
+		for(int i = 0; i <n; i++) {
+			
+			//Columns
+			for(int j = 0; j < n; j++) {
+				
+				int binary = (int)(Math.random() * 2);
+				
+				System.out.print(binary);
+			}
+			
+			System.out.print("|");
+			System.out.println();
+		}
 	}
 
 }

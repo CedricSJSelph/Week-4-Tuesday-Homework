d54789b5f5fb7df1a8200729e1e0184b0061c962
koastiebratt@gmail.com <you@example.com>
2020-08-22 14:23:10 -0400

    Unable to figure out formatting past 10 Numbers

--- src/src/TableByN.java
+++ src/src/TableByN.java
@@ -1,10 +1,34 @@
 package src;
 
+import java.util.Scanner;
+
 public class TableByN {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int n;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter a value for n: ");
+		
+		n = input.nextInt();
+		
+		displayPattern(n);
 
 	}
-
+	
+	public static void displayPattern(int n) {
+		
+		//Number of lines;
+		for(int i = 0; i<=n; i++) {
+			
+			//Print spaces and number:
+			for(int j = n; j>=1; j--) {
+				
+				System.out.print((i<j) ? " ": j);
+			}
+			System.out.println();
+		}
+	}
 }
